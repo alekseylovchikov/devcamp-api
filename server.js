9a8754ea1c3f8f0a54b362fde61672115d5e5047
Aleksey Lovchikov <aleksey.lovchikov@gmail.com>
2021-01-17 13:24:50 +0300

    added controllers and routes

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
+const bootcamps = require('./routes/bootcamps');
+
+app.use('/api/v1/bootcamps', bootcamps);
 
 app.listen(
   PORT,
